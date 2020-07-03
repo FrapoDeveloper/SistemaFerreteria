d2c212fdc038ceee83be9e143eba5e8f1f7917a8
Fran Espino <fran_espino189@outlook.com>
2020-07-03 18:30:59 -0500

    first commit

---
+++ SistemaFerreteria2020/login.cpp
@@ -0,0 +1,46 @@
+#include "login.h"
+#include "ui_login.h"
+#include "menu.h"
+
+Login::Login(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Login)
+{
+    ui->setupUi(this);
+    ui->id_ingresar->setStyleSheet("* { background-color: rgb(219,101,211,86) }");
+    ui->mensaje_error->setStyleSheet("QLabel {color: rgb(255,0,35) }");
+    ui->mensaje_error->setText("");
+    setFixedSize(ui->ventana_login->sizeHint());
+    setWindowTitle("Login");
+
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+
+void Login::on_id_ingresar_clicked()
+{
+
+   QString user = "admin2020";
+
+   QString pass = "root123";
+
+    if( ui->name_user->text() == "" &&  ui->pass_user->text()==""){
+        ui->mensaje_error->setText("Debes ingresar los datos.");
+    }
+
+    if( ui->name_user->text() == user && ui->pass_user->text() == pass){
+        Menu * next = new Menu();
+        next->show();
+        close();
+    }else{
+           ui->mensaje_error->setText("Usuario o contaseña incorrecta.");
+    }
+
+
+
+
+}
